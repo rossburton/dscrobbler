ea3a1d60d895a52f4e5c366e159bb67abfdb458d
Ross Burton <ross@burtonini.com>
2008-11-29 19:32:52 +0000

    Cleanup whitespace

--- server.py
+++ server.py
@@ -14,13 +14,13 @@ class ScrobblerService(dbus.service.Object):
         client = gconf.client_get_default ()
         username = client.get_string("/apps/dscrobbler/username")
         password = client.get_string("/apps/dscrobbler/password")
-        
+
         self.scrobbler = Scrobbler(username, password)
-        
+
         self.timeout_id = 0
-    
+
     # TODO: def NowPlaying()
-    
+
     @dbus.service.method(
         dbus_interface='com.burtonini.Scrobbler',
         in_signature='usssuuss',
@@ -63,5 +63,5 @@ if __name__ == "__main__":
     session_bus = dbus.SessionBus()
     name = dbus.service.BusName("com.burtonini.Scrobbler", session_bus)
     s = ScrobblerService(name)
-    
+
     gobject.MainLoop().run()
