6489c545e267ea22b7594e8bb527172d7cb19d69
Ross Burton <ross@burtonini.com>
2008-12-10 11:44:02 +0000

    Add basic DEntry struct

---
+++ src/dentry.h
@@ -0,0 +1,15 @@
+#include <time.h>
+
+#define EXTRA_URI_ENCODE_CHARS "&+"
+
+typedef struct {
+  char *artist;
+  char *album;
+  char *title;
+  unsigned int length;
+  char *mbid;
+  time_t play_time;
+} DEntry;
+
+void d_entry_init (DEntry *entry);
+void d_entry_free (DEntry *entry);
