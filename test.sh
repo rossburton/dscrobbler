53558cf7157cdb9e82a4c0c4825934aa05cc439c
Ross Burton <ross@burtonini.com>
2009-01-08 21:52:02 +0000

    Update test to match current api

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 dbus-send  --print-reply --dest=com.burtonini.Scrobbler /com/burtonini/Scrobbler com.burtonini.Scrobbler.Submit \
-    uint32:`date +%s` string:"José González" string:"Killing For Love" uint32:3 uint32:182
+    uint32:`date +%s` string:"José González" string:"Killing For Love" uint32:3 uint32:182 string:"" string:"" string:"P"
 
 #dbus-send  --print-reply --dest=com.burtonini.Scrobbler /com/burtonini/Scrobbler com.burtonini.Scrobbler.Flush
