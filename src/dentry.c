89cc462abcc67015d1b0f8c4ddaf580c1e1b2a22
Ross Burton <ross@burtonini.com>
2009-01-09 19:49:31 +0000

    Remove d_entry_init

--- src/dentry.c
+++ src/dentry.c
@@ -2,17 +2,6 @@
 #include "dentry.h"
 
 void
-d_entry_init (DEntry *entry)
-{
-  entry->artist = g_strdup ("");
-  entry->album = g_strdup ("");
-  entry->title = g_strdup ("");
-  entry->length = 0;
-  entry->play_time = 0;
-  entry->mbid = g_strdup ("");
-}
-
-void
 d_entry_free (DEntry *entry)
 {
   g_free (entry->artist);
--- src/dentry.h
+++ src/dentry.h
@@ -11,5 +11,4 @@ typedef struct {
   time_t play_time;
 } DEntry;
 
-G_GNUC_DEPRECATED void d_entry_init (DEntry *entry);
 void d_entry_free (DEntry *entry);
